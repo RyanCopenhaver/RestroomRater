f6fc150149b2a2ba31166ae9b9cca1d8300f695b
SKrauth <spkrauth@gmail.com>
2018-07-05 18:57:23 -0700

    fixing missed compiler error

--- RestroomRaterApp/src/app/models/review.ts
+++ RestroomRaterApp/src/app/models/review.ts
@@ -10,7 +10,7 @@ export class Review {
     public rating: number,
     public timestamp: number,
     public geoLocation?: Location,
-    public place?: Place
+    //public place?: Place
   ) { }
 
 }
