0e429835578c54cc31e43951d6a03ebe799d8ac7
SKrauth <spkrauth@gmail.com>
2018-07-17 18:15:32 -0700

    places input

--- RestroomRaterApp/src/app/place-input/place-input.component.ts
+++ RestroomRaterApp/src/app/place-input/place-input.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {LocationService} from '../location.service';
 import {LocationComponent} from '../location/location.component'
 import {Location} from '../models/location';
+import {Place} from '../models/place';
 
 @Component({
   selector: 'app-place-input',
@@ -12,12 +13,26 @@ import {Location} from '../models/location';
 export class PlaceInputComponent implements OnInit {
 
   @ViewChild(LocationComponent) locationComponent;
+  possiblePlaces: Place[];
 
   constructor(private locationService: LocationService) { }
 
   // onchange this.locationComponent.currentLocation
   getCurrentLocation(): Location {
     return this.locationComponent.currentLocation
+  }
+
+  updatePlaces(text){
+    var currentLocation = this.getCurrentLocation();
+    this.possiblePlaces = [];
+
+    this.locationService.callPlacesApi(text, currentLocation).subscribe(
+      res => {
+        res.results.forEach(item => {
+          this.possiblePlaces.push(new Place(item.id, item.name, item.formatted_address))
+        })
+      }
+    );
   }
 
   ngOnInit() {
